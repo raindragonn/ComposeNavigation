67c38aeaef40a61d70f7ba66b4d1afbc3d55019c
raindragonn <raindragonn@gmail.com>
2025-04-15 09:55:42 +0900

    컴포즈 네비게이션 라이브러리 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,16 +2,17 @@ plugins {
 	alias(libs.plugins.android.application)
 	alias(libs.plugins.kotlin.android)
 	alias(libs.plugins.kotlin.compose)
+	alias(libs.plugins.kotlin.serialization)
 }
 
 android {
 	namespace = "com.raindragonn.composenavigation"
-	compileSdk = 34
+	compileSdk = 35
 
 	defaultConfig {
 		applicationId = "com.raindragonn.composenavigation"
 		minSdk = 24
-		targetSdk = 34
+		targetSdk = 35
 		versionCode = 1
 		versionName = "1.0"
 
@@ -49,6 +50,9 @@ dependencies {
 	implementation(libs.androidx.ui.graphics)
 	implementation(libs.androidx.ui.tooling.preview)
 	implementation(libs.androidx.material3)
+
+	implementation(libs.androidx.navigation.compose)
+	implementation(libs.kotlinx.serialization.json)
 	testImplementation(libs.junit)
 	androidTestImplementation(libs.androidx.junit)
 	androidTestImplementation(libs.androidx.espresso.core)
